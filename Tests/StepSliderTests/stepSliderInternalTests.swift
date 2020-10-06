c55686adb9d6d35c5f7d86676d84d8ad75142aeb
Tsungyu Yu <ytyubox@gmail.com>
2020-10-06 11:08:54 +0800

    make sure that init will get the correct default value

--- Tests/StepSliderTests/stepSliderInternalTests.swift
+++ Tests/StepSliderTests/stepSliderInternalTests.swift
@@ -1,9 +1,38 @@
 import XCTest
 @testable import StepSlider
 final class stepSliderInternalTests: XCTestCase {
-    func test_sliderLayoutLayersAnimaetd() throws {
+    func test_slider_init_get_correct_default_value() throws {
         let sut = StepSlider()
-        sut.layoutLayersAnimated(true)
+        XCTAssertEqual(sut.maxCount, 4)
+        XCTAssertEqual(sut.index, 0)
+        XCTAssertEqual(sut.trackHeight, 4)
+        XCTAssertEqual(sut.trackCircleRadius, 5)
+        XCTAssertEqual(sut.sliderCircleRadius, 12.5)
+        XCTAssertEqual(sut.dotsInteractionEnabled, true)
+        XCTAssertEqual(sut.isDotsInteractionEnabled, true)
+        XCTAssertEqual(sut.trackColor, .init(white: 0.41, alpha: 1))
+        XCTAssertEqual(sut.sliderCircleColor, .white)
+        XCTAssertEqual(sut.sliderCircleImage, .none)
+        XCTAssertEqual(sut.labels, [])
+        XCTAssertEqual(sut.labelFont, .systemFont(ofSize: 15))
+        XCTAssertEqual(sut.labelColor, .none)
+        XCTAssertEqual(sut.labelOffset, 20)
+        XCTAssertEqual(sut.labelOrientation, .down)
+        XCTAssertEqual(sut.adjustLabel, true)
+        XCTAssertEqual(sut.enableHapticFeedback, true)
+        #warning("No idea how to test _trackLayer on creation")
+//        XCTAssertEqual(sut._trackLayer, CAShapeLayer())
+        #warning("No idea how to test _sliderCircleLayer on creation")
+//        XCTAssertEqual(sut._sliderCircleLayer, CAShapeLayer())
+        XCTAssertEqual(sut._trackCirclesArray, [])
+        XCTAssertEqual(sut._trackLabelsArray, [])
+        XCTAssertEqual(sut._trackCircleImages, [:])
+        XCTAssertEqual(sut._selectFeedback, nil)
+        XCTAssertEqual(sut.animateLayouts, true)
+        XCTAssertEqual(sut.maxRadius, 12.5)
+        XCTAssertEqual(sut.diff, 0)
+        XCTAssertEqual(sut.startTouchPosition, .zero)
+        XCTAssertEqual(sut.startSliderPosition, .zero)
+        XCTAssertEqual(sut.contentSize, .zero)
     }
-
 }
